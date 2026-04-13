d05974e5a5c3542ebabdd03f4d438fe9aed78764
andreinik1 <sa.korolev.nik@gmail.com>
2026-04-13 12:57:52 +0300

    fix routes

--- sbornik-app/vite.config.ts
+++ sbornik-app/vite.config.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/physicsLab2/",
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  css: {
+    devSourcemap: true,
+  }
 })
