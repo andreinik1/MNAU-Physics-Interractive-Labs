fc2f1e6cae48e288c346f882839a868ffd611961
andreinik1 <sakorolevnik@gmail.com>
2026-05-31 15:18:59 +0300

    rebase browser to hash router SPA

--- sbornik-app/src/main.tsx
+++ sbornik-app/src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import 'katex/dist/katex.min.css';
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/MNAU-Physics-Interractive-Labs/">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
