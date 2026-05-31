d33e5c2bf1a67bdc7bd88b0f3e9e4bcd1f05e828
andreinik1 <sakorolevnik@gmail.com>
2026-05-31 14:58:26 +0300

    fix path

--- sbornik-app/src/main.tsx
+++ sbornik-app/src/main.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/physicsLab2">
+  <BrowserRouter basename="/MNAU-Physics-Interractive-Labs/">
     <App />
   </BrowserRouter>
 );
